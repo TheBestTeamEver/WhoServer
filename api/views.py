c5354124258d6b82651fbee9dfd639b951f723d1
Stanislab-BMSTU <stanislav.bmstu@mail.ru>
2016-05-20 16:45:48 +0300

    Добавил авторизацию и регистрацию

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ import json
 from django.http import HttpResponse
 from django.http.response import JsonResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
 from api.models import Level, SimpleUser
@@ -40,14 +41,43 @@ def get_all(request):
     return render(request, 'api/levels.html', context)
 
 
+@csrf_exempt
 @require_http_methods(["POST"])
 def registration(request):
-    return None
+    login = request.POST.get('login')
+    password = request.POST.get('password')
+    name = request.POST.get('name')
+    if login or password or name is None:
+        response = {'data': 'No some fields'}
+        return JsonResponse(response)
+
+    try:
+        SimpleUser.objects.get(login=login)
+        response = {'data': 'Login is already used'}
+    except Exception:
+        SimpleUser(login=login, password=password, name=name).save()
+        response = {'data': 'Ok'}
+
+    return JsonResponse(response)
 
 
+@csrf_exempt
 @require_http_methods(["POST"])
 def authentication(request):
-    return None
+    login = request.POST.get('login')
+    password = request.POST.get('password')
+
+    if login or password is None:
+        response = {'data': 'No some fields'}
+        return JsonResponse(response)
+
+    try:
+        SimpleUser.objects.get(login=login, password=password)
+        response = {'data': 'Ok'}
+    except Exception:
+        response = {'data': 'Wrong login/password'}
+
+    return JsonResponse(response)
 
 
 def get_top(request):
