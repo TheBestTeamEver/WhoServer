386e0368b811845d6565f9ec1918879e4ebe148c
MaximZemskov <m.zemskov1@gmail.com>
2016-05-22 23:34:22 +0300

    configs

--- who/settings.py
+++ who/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k25!f6kc_e+$@+vx5rfryx0%r0b7=xt)@!!f&kt4ne!m8%9vd3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = []
 
