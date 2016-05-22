af6b5c83988b9dfd17c5822209334428bcf94114
Stanislab-BMSTU <stanislav.bmstu@mail.ru>
2016-05-23 02:02:45 +0300

    what?

--- who/settings.py
+++ who/settings.py
@@ -4,7 +4,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = []
 
